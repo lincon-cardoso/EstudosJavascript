ac213a3d51c4ea32cec3c3d0c4a87d586c8cfe59
lincon-cardoso <59518299+lincon-cardoso@users.noreply.github.com>
2022-09-28 13:13:18 -0300

    Update app.js

---
+++ node/aula1/app.js
@@ -0,0 +1,12 @@
+const nome = 'luiz';
+const sobrenome = 'miranda';
+
+const falaNome = () => {
+    console.log(nome,sobrenome);
+}
+
+exports.nome = nome;
+exports.sobrenome = sobrenome;
+exports.falaNome = falaNome;
+
+console.log(exports);
